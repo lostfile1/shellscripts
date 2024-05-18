5b5ceac8f1caae343b41088fd620dcf99d91383f
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-05-18 09:11:23 -0400

    Update posback.sh

--- posback.sh
+++ posback.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+# this script is for backing up your postgres database at the first day of every month
+# i dont quite have this one on my server yet 
+
+pg_dump prod-db -U postgres > prod-$(date +%m-%y).sql
+tar -rvf prod-$(date +%m-%y).sql 
