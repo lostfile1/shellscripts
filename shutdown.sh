ff7bbf013c207e67e1bddf8dc10eed5d03a71914
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-05-18 11:08:51 -0400

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # shut it down for good Im either to old or dead
-# if not just echo something
+# if not just echo something hey im not going to
+# keep shit running when im old and feeble fuck that
 
 var=$(date +%Y)
 if [ $var = 2074 ]
