d55432c74f94d45db4af4125b89ac220754e6dca
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-05-18 09:44:07 -0400

    Update posback.sh

--- posback.sh
+++ posback.sh
@@ -2,5 +2,5 @@
 # this script is for backing up your postgres database at the first day of every month
 # i dont quite have this one on my server yet 
 
-pg_dump prod-db -U postgres > prod-$(date +%m-%y).sql
-tar -rvf backup.tar prod-$(date +%m-%y).sql 
+pg_dump synapse > synapse-$(date +%m-%y).sql
+tar -rvf backup.tar synapse-$(date +%m-%y).sql 
