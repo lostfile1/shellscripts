52c7c206dca4008d71fc134ee0955faccf115cae
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-05-18 10:10:11 -0400

    Update posback.sh

--- posback.sh
+++ posback.sh
@@ -2,8 +2,8 @@
 # this script is for backing up a matrix server database
 # it runs on the first day of the month and packs it into a tar file
 
-pg_dump synapse > synapse-$(date +%m-%Y).sql
-pg_dump gochan > gochan-$(date +%m-%Y).sql
-tar -rvf backup.tar synapse-$(date +%m-%Y).sql
-tar -rvf backup.tar gochan-$(date +%m-%Y).sql
+pg_dump synapse > synapse-$(date +%m).sql
+pg_dump gochan > gochan-$(date +%m).sql
+tar -rvf backup.tar synapse-$(date +%m).sql
+tar -rvf backup.tar gochan-$(date +%m).sql
 rm *.sql
