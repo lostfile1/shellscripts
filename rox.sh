db2ea77b05aacaf77c80b6277e5a55572e2d4982
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-07-04 17:19:04 -0400

    Update rox.sh

--- rox.sh
+++ rox.sh
@@ -4,3 +4,4 @@ exec /home/whitepaperkat/.local/share/ROX-Filer/AppRun "$@"
 
 # this is what i use to start my install of rox file manager
 # and yes i got this to compile on the latest verson of arch
+# why you ask because i wanted a file manager that was retro looking
