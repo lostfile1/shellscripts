cc4db88e33d82a81084d76ce0813bf800c0b5d6c
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2024-07-04 16:51:20 -0400

    Update rox.sh
    added more comments

--- rox.sh
+++ rox.sh
@@ -3,3 +3,4 @@
 exec /home/whitepaperkat/.local/share/ROX-Filer/AppRun "$@"
 
 # this is what i use to start my install of rox file manager
+# and yes i got this to compile on the latest verson of arch
